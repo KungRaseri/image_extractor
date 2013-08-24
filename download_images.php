e76e2d48920c043feadf91748940822538708e09
Joshua Homer <countjoshington@gmail.com>
2013-08-24 03:25:40 -0600

    testing the downloading - had to add ../static due to the folder movement put in sleeps to slow the script down pulled out some of the other deck "views"

--- download_images.php
+++ download_images.php
@@ -1,5 +1,7 @@
 <?php
-	$view_urls = array("viewFFO", "viewFF4", "viewFF5", "viewFF6", 
+	ob_implicit_flush(true);
+	// "viewFFO", "viewFF4", "viewFF5", 
+	$view_urls = array("viewFF6", 
 					   "viewFF7", "viewFF8", "viewFF8r", "viewFF9", 
 					   "viewFFX", "viewFFX2", "viewFFXI", "viewFFXII", 
 					   "viewFFXIII", "viewFFT", "viewCT", "viewDL", 
@@ -7,7 +9,7 @@
 					   "viewSE", "questcards");
 	$dom = new DOMDocument();
     $base_url = "http://ttadvance.ca/";
-	$local_base_url = "/static/finalfantasy/images/";
+	$local_base_url = "../static/finalfantasy/images/";
 	$count = 0;
 	$skipped_count = 0;
 	if(isset($_POST['start']))
@@ -19,7 +21,7 @@
 			echo "Extracting images from: ";
 			echo $web_url . "<br />";
 			echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=<br />";
-			@$dom->loadHTMLFile($web_url);
+			$dom->loadHTMLFile($web_url);
 			foreach ($dom->getElementsByTagName('img') as $img)
 			{
 				$image_url = $base_url . $img->getAttribute('src');
@@ -27,7 +29,7 @@
 				if(!file_exists($local_path))
 				{
 					echo "Downloading Image: " . $image_url . "<br />";
-				    // file_put_contents($local_path, file_get_contents($image_url));
+				    file_put_contents($local_path, file_get_contents($image_url));
 					$count++;
 				}
 				else
@@ -40,7 +42,7 @@
 				if(!file_exists($local_path))
 				{
 					echo "Downloading Hover Image: " . $hover_image_url . "<br />";
-				    // file_put_contents($local_path, file_get_contents($hover_image_url));
+				    file_put_contents($local_path, file_get_contents($hover_image_url));
 					$count++;
 				}
 				else
@@ -48,7 +50,9 @@
 					echo $local_path . " already exists! Skipping... <br />";
 					$skipped_count++;
 				}
+				sleep(2);
 			}
+			sleep(5);
 		}
 		echo $count . " items downloaded.<br />";
 		echo $skipped_count . " items skipped.<br />";
